4d9e1ceaaf30b8b9205927a693c7eea65f76eede
Cary Landers <cary.landers@outlook.com>
2016-09-02 16:15:45 -0500

    Update installmc.sh

--- installmc.sh
+++ installmc.sh
@@ -1,5 +1,5 @@
 sudo apt-get install apache2 screen php libapache2-mod-php php-gd openjdk-8-jre git
-mkdir server
+mkdir /home/administrator/server
 chmod -R 777 server
 git clone https://github.com/Alanaktion/MCHostPanel.git /var/www/html/MCHostPanel
 sudo chmod -R 777 /var/www/html/MCHostPanel
